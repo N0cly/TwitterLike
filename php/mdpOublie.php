3b69e2368a3a6786e4e3a9022305ee8e2efb2121
Nocly <45365883+N0cly@users.noreply.github.com>
2023-09-20 22:38:35 +0200

    code provisoire save dans la db

--- php/mdpOublie.php
+++ php/mdpOublie.php
@@ -1,14 +1,21 @@
 
 
-<?php 
+<?php
 //Avant de pouvoir l'utiliser, nous devons vérifier les points suivants :
 //La fonction mail() est bien activée par l'hébergeur.
 //Pour le vérifier, il suffit de regarder son phpinfo() : Le serveur SMTP est correctement configuré.
 
 if (isset($_SERVER['REQUEST_METHOD']) &&
-$_SERVER["REQUEST_METHOD"] == "POST") {
+    $_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer l'adresse e-mail soumise par l'utilisateur
     $email = $_POST["email"];
+
+    try {
+        $db = new PDO('sqlite:../db/db_nexa.sqlite');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        die('Erreur de connexion à la base de données : ' . $e->getMessage());
+    }
 
     // Vérifier si l'adresse e-mail existe dans la base de données (vous devez implémenter cette vérification)
 
@@ -19,29 +26,33 @@ $_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($stmt->rowCount() < 0) {
         // L'email n'éxiste pas dans la bd, affichez un message d'erreur ou redirigez l'utilisateur
-        header('Location: inscription.php?erreur=email_existe');
-        echo "email existe pas";
+        header('Location: inscription.php?erreur=non_inscrit');
+//        echo "email existe pas";
+        exit;
+    } else {
+
+        // Générer un code de réinitialisation aléatoire
+        $uniqid = uniqid( true);
+        $code = strtoupper(substr($uniqid, -5));
+
+        $query = "UPDATE users SET codeMDPOublie = :code WHERE email = :email";
+        $stmt = $db->prepare($query);
+        $stmt->bindParam(':code', $code, PDO::PARAM_STR);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+        $stmt->execute();
+
+
+        // Envoyer l'e-mail avec le code de réinitialisation==
+        $subject = "Réinitialisation de mot de passe";
+        $message = "Votre code de réinitialisation de mot de passe est : " . $code;
+        $headers = "From: no-replay@nexa.nocly.fr";
+        mail($email, $subject, $message, $headers);
+
+        // Rediriger l'utilisateur vers la page de réinitialisation de mot de passe
+        header("Location: reinitialisationMDP.php");
         exit;
     }
 
-    // Générer un code de réinitialisation aléatoire
-    $code = uniqid();
-
-    // Stocker le code de réinitialisation dans la base de données (vous devez implémenter cette étape)
-    $query = "INSERT INTO users (codeMDPOublie) VALUES ($code)";
-    $stmt = $db->prepare($query);
-    $stmt->bindParam(':code', $code, PDO::PARAM_STR);
-    $stmt->execute();
-
-    // Envoyer l'e-mail avec le code de réinitialisation
-    $subject = "Réinitialisation de mot de passe";
-    $message = "Votre code de réinitialisation de mot de passe est : " . $code;
-    $headers = "From: thibaultamettler@gmail.com";
-    mail($email, $subject, $message, $headers);
-
-    // Rediriger l'utilisateur vers la page de réinitialisation de mot de passe
-    header("Location: reinitialisationMDP.php");
-    exit;
 }
 ?>
 
@@ -59,13 +70,13 @@ $_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 
 <body>
-    <h2>Mot de passe oublié</h2>
-    <p>Entrez votre adresse e-mail pour recevoir un code de réinitialisation.</p>
-    <form method="post">
-        <label for="email">Adresse e-mail :</label>
-        <input type="email" name="email" required>
-        <input type="submit" value="Envoyer le code de réinitialisation">
-    </form>
+<h2>Mot de passe oublié</h2>
+<p>Entrez votre adresse e-mail pour recevoir un code de réinitialisation.</p>
+<form method="post">
+    <label for="email">Adresse e-mail :</label>
+    <input type="email" name="email" required>
+    <input type="submit" value="Envoyer le code de réinitialisation">
+</form>
 </body>
 
 </html>
