9261af2ba94f82a6ba8bdeeb5b72af5769689d35
N0cly <enzo.bedos13@gmail.com>
2023-09-26 15:10:33 +0200

    update css

--- js/script.js
+++ js/script.js
@@ -1,45 +1,4 @@
 
-
-const inscriptionButton = document.getElementById("inscriptionButton");
-const connexionButton = document.getElementById("connexionButton");
-const formInscription = document.getElementById("register");
-const formConnexion = document.getElementById("connexion");
-formConnexion.style.display = "block";
-formInscription.style.display = "none";
-
-
-// Gestionnaire d'événement pour le bouton "Inscription"
-inscriptionButton.addEventListener("click", function () {
-    formInscription.style.display = "block";
-    formConnexion.style.display = "none";
-});
-
-// Gestionnaire d'événement pour le bouton "Connexion"
-connexionButton.addEventListener("click", function () {
-    formInscription.style.display = "none";
-    formConnexion.style.display = "block";
-});
-
-
-
-// function showPwd1() {
-//     var x = document.getElementById("mdp1");
-//     if (x.type === "password") {
-//         x.type = "text";
-//     } else {
-//         x.type = "password";
-//     }
-// }
-//
-// function showPwd2() {
-//     var x = document.getElementById("mdp2");
-//     if (x.type === "password") {
-//         x.type = "text";
-//     } else {
-//         x.type = "password";
-//     }
-// }
-
 function togglePasswordVisibility(id) {
     var x = document.getElementById(id);
     if (x.type === "password") {
@@ -62,3 +21,4 @@ function afficherFormulaire(formulaire) {
         formulaireConnexion.style.display = "block";
     }
 }
+
--- views/accueil.php
+++ views/accueil.php
@@ -6,7 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Page de connexion">
     <meta name="author" content="Henricy Limosani Safran Amettler Zoppi Bedos">
-    <link href="../css/style1.css" rel="stylesheet" type="text/css">
+    <!--<link href="../css/style1.css" rel="stylesheet" type="text/css">-->
+    <link href="../css/accueil.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
     <link rel="icon" href="../Images/Logos/Logo_Nexa-smaller.png">
     <title>Nexa</title>
 </head>
@@ -19,72 +22,72 @@
             <h2>Accueil Nexa</h2>
         </section>
 
+        <section class="accueil">
 
-        <section class="choice-form">
-            <section class="choice-form__button">
-                <button class="inscriptionButton" id="inscriptionButton" onclick="afficherFormulaire('inscription')">Inscription</button>
-                <button class="connexionButton" id="connexionButton" onclick="afficherFormulaire('connexion')">Connexion</button>
+            <section class="choice-form">
+                <section class="choice-form__button">
+                    <button class="inscriptionButton button" id="inscriptionButton" onclick="afficherFormulaire('inscription')">Inscription</button>
+                    <button class="connexionButton button" id="connexionButton" onclick="afficherFormulaire('connexion')">Connexion</button>
+                </section>
+                <div id="message-erreur"></div>
             </section>
-            <div id="message-erreur"></div>
+
+
+            <form  id="register" class="register" action="index.php?user=registerUser" method="post"> <!-- Formulaire inscription -->
+                <h3>Inscription</h3>
+                <div class="field">
+                    <label for="username">Username :</label>
+                    <input type="text" id="username" name="username" class="field" required>
+                </div>
+
+                <div class="field">
+                    <label for="email">Email :</label>
+                    <input type="email" id="email" name="email" class="field field-email" required>
+                </div>
+
+                <div class="field ">
+                    <label for="mot_de_passe">Mot de passe :</label>
+                    <section class="field-pswd">
+                        <input type="password" id="mdp1" name="mot_de_passe" class="mdp field field-pswd__mdp" required>
+                        <input type="checkbox" class="checkpswd" onclick="togglePasswordVisibility('mdp1')">
+                    </section>
+                </div>
+
+                <div class="inscription-connexion-button-container">
+                    <input class="inscriptionButton" type="submit" value="S'inscrire">
+                </div>
+
+            </form>
+
+
+
+            <form id="connexion" class="connexion" action="index.php?user=loginUser" method="post"> <!-- Formulaire connexion -->
+                <h3>Connexion</h3>
+                <div class="field">
+                    <label for="email">Email or Username :</label>
+                    <input class="field field-email" type="text" name="email" required><br>
+                </div>
+
+                <div class="field ">
+                    <label for="mot_de_passe">Mot de passe :</label>
+                    <section class="field-pswd">
+                        <input type="password" name="mot_de_passe" id="mdp2" class="mdp field field-pswd__mdp" required>
+                        <input type="checkbox" class="checkpswd" onclick="togglePasswordVisibility('mdp2')">
+                    </section>
+                </div>
+
+                <a href="views/forgotPwd.php">Mot de passe oublié</a>
+
+                <div class="inscription-connexion-button-container">
+                    <input class="connexionButton" type="submit" value="Se connecter">
+                </div>
+            </form>
         </section>
-
-
-
-
-        <form  id="register" class="register" action="index.php?user=registerUser" method="post"> <!-- Formulaire inscription -->
-            <h3>Inscription</h3>
-            <div class="field">
-                <label for="username">Username :</label>
-                <input type="text" id="username" name="username" class="field" required>
-            </div>
-
-            <div class="field">
-                <label for="email">Email :</label>
-                <input type="email" id="email" name="email" class="field field-email" required>
-            </div>
-
-            <div class="field ">
-                <label for="mot_de_passe">Mot de passe :</label>
-                <section class="field-pswd">
-                    <input type="password" id="mdp1" name="mot_de_passe" class="mdp field field-pswd__mdp" required>
-                    <input type="checkbox" class="checkpswd" onclick="togglePasswordVisibility('mdp1')">
-                </section>
-            </div>
-
-            <div class="inscription-connexion-button-container">
-                <input class="inscriptionButton" type="submit" value="S'inscrire">
-            </div>
-
-        </form>
-
-
-
-        <form id="connexion" class="connexion" action="index.php?user=loginUser" method="post"> <!-- Formulaire connexion -->
-            <h3>Connexion</h3>
-            <div class="field">
-                <label for="email">Email or Username :</label>
-                <input class="field field-email" type="text" name="email" required><br>
-            </div>
-
-            <div class="field ">
-                <label for="mot_de_passe">Mot de passe :</label>
-                <section class="field-pswd">
-                    <input type="password" name="mot_de_passe" id="mdp2" class="mdp field field-pswd__mdp" required>
-                    <input type="checkbox" class="checkpswd" onclick="togglePasswordVisibility('mdp2')">
-                </section>
-            </div>
-
-            <a href="views/forgotPwd.php">Mot de passe oublié</a>
-
-            <div class="inscription-connexion-button-container">
-                <input class="connexionButton" type="submit" value="Se connecter">
-            </div>
-        </form>
     </section>
 </section>
-<section class="NexaInformation">
+<footer class="NexaInformation">
     <h2>A Propos de Nexa</h2>
-</section>
+</footer>
 <script src="../js/script.js"></script>
 </body>
 </html>
