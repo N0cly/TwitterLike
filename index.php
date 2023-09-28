6876f4e5d07a99620792a74fd1b577d3a3669c42
N0cly <enzo.bedos13@gmail.com>
2023-09-28 16:34:21 +0200

    rewrite url

--- index.php
+++ index.php
@@ -11,23 +11,36 @@ use ctrl\UserController;
 require 'models/userModel.php';
 
 
+// Récupérer l'URI de la demande
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// Supprimer les paramètres de l'URL
+$cleanUri = strtok($requestUri, '?');
+
+// Divisez l'URI en segments en utilisant '/'
+$uriSegments = explode('/', trim($cleanUri, '/'));
+
+$controllerName = isset($uriSegments[0]) ? ucfirst($uriSegments[0]) . 'Controller' : 'AccueilController';
+$action = isset($uriSegments[1]) ? $uriSegments[1] : 'accueil';
+
 // Récupérer le nom du contrôleur depuis l'URL
-$controllerName = isset($_GET['Controller']) ? $_GET['Controller'] : 'Accueil';
+//$controllerName = isset($_GET['Controller']) ? $_GET['Controller'] : 'Accueil';
 
 // Ajoutez ici la logique pour valider et sécuriser le nom du contrôleur, par exemple, en vérifiant qu'il correspond à un contrôleur existant.
 
 // Inclure le fichier du contrôleur spécifié
-$controllerFile ="ctrl/{$controllerName}Controller.php";
+$controllerFile = "ctrl/{$controllerName}.php";
+//$controllerFile ="ctrl/{$controllerName}Controller.php";
 
 if (file_exists($controllerFile)) {
     require $controllerFile;
 
     // Instancier le contrôleur
-    $controllerClassName = $controllerName . 'Controller';
-    $controller = new $controllerClassName();
+    //$controllerClassName = $controllerName . 'Controller';
+    $controller = new $controllerName();
 
     // Récupérer la méthode depuis l'URL
-    $action = isset($_GET['action']) ? $_GET['action'] : 'accueil';
+   // $action = isset($_GET['action']) ? $_GET['action'] : 'accueil';
 
     // Vérifier si la méthode existe dans le contrôleur
     if (method_exists($controller, $action)) {
