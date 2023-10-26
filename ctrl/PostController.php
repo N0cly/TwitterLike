9310987f94854e6bd2eac141b07bb531affb216a
TibOwO <thibaultamettler@gmail.com>
2023-10-26 13:15:36 +0200

    c'est bugé mais moins que tt a l'heure je save

--- ctrl/PostController.php
+++ ctrl/PostController.php
@@ -7,18 +7,25 @@ require_once $_SERVER['DOCUMENT_ROOT'] . ('/models/PostModel.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 class PostController
 {
 
     public function getPost($id_post)
     {
-        $postModel = new PostModel();
-        $post = $postModel->getPost($id_post);
+        if (
+            isset($_SERVER['REQUEST_METHOD']) &&
+            $_SERVER['REQUEST_METHOD'] == "GET"
+        ) {
+            $user = $_SESSION['username'];
+            $postModel = new PostModel();
+            $post = $postModel->getPost($id_post);
 
-        $post['LikeCount'] = $postModel->getPostLikes($id_post);
-        $post['user_liked'] = $postModel->getPostUserLiked($id_post, $_SESSION['user']);
+            $post['LikeCount'] = $postModel->getPostLikes($id_post);
+            $post['user_liked'] = $postModel->getPostUserLiked($id_post, $user);
 
-        return $post;
+            return $post;
+        }
     }
 
     public function getPosts()
@@ -42,6 +49,21 @@ class PostController
 
             $postModel = new PostModel();
             $postModel->createPost($user, $content, $image, $categorie);
+        }
+    }
+
+    public function sendComment()
+    {
+        if (isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == "POST") {
+            $image = null; // Si vous n'utilisez pas l'image pour le moment
+            $content = $_POST["contenu"];
+            session_start();
+            // Obtenez l'username à partir de la session
+            $user = $_SESSION['username'];
+            $id_pere = $_SESSION['post_id'];
+
+            $postModel = new PostModel();
+            $postModel->commentPost($user, $content, $image, $id_pere);
         }
     }
 
--- views/dashboard.php
+++ views/dashboard.php
@@ -143,54 +143,57 @@ $is_moderator = $user_data['is_moderator'];
                         </h3>
                     </div>
                     <hr class="post-divider">
-                    <p class="post-content">
-                        <?php echo $post['contenu']; ?>
-                    </p>
-                    <?php if (!empty($post['image'])): ?>
-                        <img src="../<?php echo $post['image']; ?>" alt="Photo de profil" class="post-image" />
-                    <?php endif; ?>
-                    <div class="post-actions">
-                        <button class="post-action like" data-id_post="<?php echo $post['id_post']; ?>" <?php echo $post['user_liked'] > 0 ? 'disabled' : ''; ?>>❤️
-                            <?php echo $post['LikeCount']; ?>
-                        </button>
-                        <button class="post-action comment" data-id_post="<?php echo $post['id_post']; ?>">💬
-                            <?php echo $post['Comment']; ?>
-                        </button>
-                        <button class="post-action share">🔗
-                            <?php echo $post['partage']; ?>
-                        </button>
-                        <?php if ($is_moderator == 1): ?>
-                            <button class="post-action delete" data-id_post="<?php echo $post['id_post']; ?>">🗑</button>
+
+                    <a href="post.php?id=<?php echo $post['id_post']; ?>">
+                        <p class="post-content">
+                            <?php echo $post['contenu']; ?>
+                        </p>
+                        <?php if (!empty($post['image'])): ?>
+                            <img src="../<?php echo $post['image']; ?>" alt="Photo de profil" class="post-image" />
                         <?php endif; ?>
-                    </div>
-                    <div id="commentModal<?php echo $post['id_post']; ?>" class="modal">
-                        <div class="modal-content">
-                            <form action="traitement_commentaire.php" method="post" enctype="multipart/form-data">
-                                <input type="hidden" name="id_pere" id="id_pere<?php echo $post['id_post']; ?>"
-                                    value="<?php echo $post['id_post']; ?>">
-                                <div class="input-container">
-                                    <label for="contenu">Commentaire :</label>
-                                    <textarea id="contenu" name="contenu"></textarea>
-                                </div>
-                                <input type="submit" value="Commenter">
-                            </form>
+                        <div class="post-actions">
+                            <button class="post-action like" data-id_post="<?php echo $post['id_post']; ?>" <?php echo $post['user_liked'] > 0 ? 'disabled' : ''; ?>>❤️
+                                <?php echo $post['LikeCount']; ?>
+                            </button>
+                            <button class="post-action comment" data-id_post="<?php echo $post['id_post']; ?>">💬
+                                <?php echo $post['Comment']; ?>
+                            </button>
+                            <button class="post-action share">🔗
+                                <?php echo $post['partage']; ?>
+                            </button>
+                            <?php if ($is_moderator == 1): ?>
+                                <button class="post-action delete" data-id_post="<?php echo $post['id_post']; ?>">🗑</button>
+                            <?php endif; ?>
                         </div>
-                    </div>
-                    <div class="comments-container" id="comments<?php echo $post['id_post']; ?>" style="display:none;">
-                        <?php
-                        $comments = $postCtrl->getComments($post['id_post']);
-                        foreach ($comments as $comment):
-                            ?>
-                            <div class="comment">
-                                <p class="comment-content">
-                                    <?php echo $comment['contenu']; ?>
-                                </p>
-                                <p class="comment-user">
-                                    <?php echo $comment['user']; ?>
-                                </p>
+                        <div id="commentModal<?php echo $post['id_post']; ?>" class="modal">
+                            <div class="modal-content">
+                                <form action="traitement_commentaire.php" method="post" enctype="multipart/form-data">
+                                    <input type="hidden" name="id_pere" id="id_pere<?php echo $post['id_post']; ?>"
+                                        value="<?php echo $post['id_post']; ?>">
+                                    <div class="input-container">
+                                        <label for="contenu">Commentaire :</label>
+                                        <textarea id="contenu" name="contenu"></textarea>
+                                    </div>
+                                    <input type="submit" value="Commenter">
+                                </form>
                             </div>
-                        <?php endforeach; ?>
-                    </div>
+                        </div>
+                        <div class="comments-container" id="comments<?php echo $post['id_post']; ?>" style="display:none;">
+                            <?php
+                            $comments = $postCtrl->getComments($post['id_post']);
+                            foreach ($comments as $comment):
+                                ?>
+                                <div class="comment">
+                                    <p class="comment-content">
+                                        <?php echo $comment['contenu']; ?>
+                                    </p>
+                                    <p class="comment-user">
+                                        <?php echo $comment['user']; ?>
+                                    </p>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </a>
                 </div>
             <?php endforeach; ?>
         </section>
