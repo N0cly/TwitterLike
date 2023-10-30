13180c30cf119783d46d541b6f9419ad0b626129
TibOwO <thibaultamettler@gmail.com>
2023-10-30 20:34:10 +0100

    Séparation de l'affichage des post dans un fichier afficherprofil.php afin d'améliorer la maintenabilitée

--- views/dashboard.php
+++ views/dashboard.php
@@ -122,74 +122,9 @@ $is_moderator = $user_data['is_moderator'];
                 </div>
             </div>
 
-            <?php
-            // Partie commentée pour afficher les messages
-            foreach ($posts as $post): ?>
-                <a href="profileViewer.php?user=<?php echo $post['user']; ?>">
-                    <div class="post">
-                        <div class="post-header">
-                            <img src="../<?php echo $post['user_pp']; ?>" alt="Photo de profil de l'utilisateur"
-                                class="post-pp post-pp-hover">
-                            <h3 class="post-username">
-                                <?php echo $post['user']; ?>
-                            </h3>
-                        </div>
-                </a>
-                <hr class="post-divider">
 
+            <?php include('afficherpost.php'); ?>
 
-                <a href="post.php?id=<?php echo $post['id_post']; ?>">
-                    <p class="post-content">
-                        <?php echo $post['contenu']; ?>
-                    </p>
-                    <?php if (!empty($post['image'])): ?>
-                        <img src="../<?php echo $post['image']; ?>" alt="Photo de profil" class="post-image" />
-                    <?php endif; ?>
-                    <div class="post-actions">
-                        <button class="post-action like" data-id_post="<?php echo $post['id_post']; ?>" <?php echo $post['user_liked'] > 0 ? 'disabled' : ''; ?>>❤️
-                            <?php echo $post['LikeCount']; ?>
-                        </button>
-                        <button class="post-action comment" data-id_post="<?php echo $post['id_post']; ?>">💬
-                            <?php echo $post['Comment']; ?>
-                        </button>
-                        <button class="post-action share">🔗
-                            <?php echo $post['partage']; ?>
-                        </button>
-                        <?php if ($is_moderator == 1): ?>
-                            <button class="post-action delete" data-id_post="<?php echo $post['id_post']; ?>">🗑</button>
-                        <?php endif; ?>
-                    </div>
-                    <div id="commentModal<?php echo $post['id_post']; ?>" class="modal">
-                        <div class="modal-content">
-                            <form action="traitement_commentaire.php" method="post" enctype="multipart/form-data">
-                                <input type="hidden" name="id_pere" id="id_pere<?php echo $post['id_post']; ?>"
-                                    value="<?php echo $post['id_post']; ?>">
-                                <div class="input-container">
-                                    <label for="contenu">Commentaire :</label>
-                                    <textarea id="contenu" name="contenu"></textarea>
-                                </div>
-                                <input type="submit" value="Commenter">
-                            </form>
-                        </div>
-                    </div>
-                    <div class="comments-container" id="comments<?php echo $post['id_post']; ?>" style="display:none;">
-                        <?php
-                        $comments = $postCtrl->getComments($post['id_post']);
-                        foreach ($comments as $comment):
-                            ?>
-                            <div class="comment">
-                                <p class="comment-content">
-                                    <?php echo $comment['contenu']; ?>
-                                </p>
-                                <p class="comment-user">
-                                    <?php echo $comment['user']; ?>
-                                </p>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                </a>
-                </div>
-            <?php endforeach; ?>
         </section>
     </main>
 
--- views/profileViewer.php
+++ views/profileViewer.php
@@ -66,71 +66,8 @@ $is_moderator = $user_data['is_moderator'];
             <div class="profile-posts">
                 <!-- Affichage des publications de l'utilisateur -->
                 <h2>Publications récentes</h2>
-                <?php foreach ($posts as $post): ?>
-                    <a href="post.php?id=<?php echo $post['id_post']; ?>">
-                        <div class="post">
-                            <div class="post-header">
-                                <img src="../<?php echo $post['user_pp']; ?>" alt="Photo de profil de l'utilisateur"
-                                    class="post-pp post-pp-hover">
-                                <h3 class="post-username">
-                                    <?php echo $post['user']; ?>
-                                </h3>
-                            </div>
-                            <hr class="post-divider">
-                            <p class="post-content">
-                                <?php echo $post['contenu']; ?>
-                            </p>
-                            <?php if (!empty($post['image'])): ?>
-                                <img src="../<?php echo $post['image']; ?>" alt="Photo de profil" class="post-image" />
-                            <?php endif; ?>
-                            <div class="post-actions">
-                                <button class="post-action like" data-id_post="<?php echo $post['id_post']; ?>" <?php echo $post['user_liked'] > 0 ? 'disabled' : ''; ?>>❤️
-                                    <?php echo $post['LikeCount']; ?>
-                                </button>
-                                <button class="post-action comment" data-id_post="<?php echo $post['id_post']; ?>">💬
-                                    <?php echo array_key_exists('Comment', $post) ? $post['Comment'] : ''; ?>
-                                </button>
-                                <button class="post-action share">🔗
-                                    <?php echo array_key_exists('partage', $post) ? $post['partage'] : ''; ?>
-                                </button>
-                                <?php if ($is_moderator): ?>
-                                    <button class="post-action delete"
-                                        data-id_post="<?php echo $post['id_post']; ?>">🗑</button>
-                                <?php endif; ?>
-                            </div>
-                            <div id="commentModal<?php echo $post['id_post']; ?>" class="modal">
-                                <div class="modal-content">
-                                    <form action="traitement_commentaire.php" method="post" enctype="multipart/form-data">
 
-                                        <input type="hidden" name="id_pere" id="id_pere<?php echo $post['id_post']; ?>"
-                                            value="<?php echo $post['id_post']; ?>">
-                                        <div class="input-container">
-                                            <label for="contenu">Commentaire :</label>
-                                            <textarea id="contenu" name="contenu"></textarea>
-                                        </div>
-                                        <input type="submit" value="Commenter">
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="comments-container" id="comments<?php echo $post['id_post']; ?>"
-                                style="display:none;">
-                                <?php
-                                $comments = $postCtrl->getComments($post['id_post']);
-                                foreach ($comments as $comment):
-                                    ?>
-                                    <div class="comment">
-                                        <p class="comment-content">
-                                            <?php echo $comment['contenu']; ?>
-                                        </p>
-                                        <p class="comment-user">
-                                            <?php echo $comment['user']; ?>
-                                        </p>
-                                    </div>
-                                <?php endforeach; ?>
-                            </div>
-                        </div>
-                    </a>
-                <?php endforeach; ?>
+                <?php include('afficherpost.php'); ?>
             </div>
         </section>
     </main>
